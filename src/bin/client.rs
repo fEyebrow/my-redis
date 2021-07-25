edbaf98ae2786dd7ca4c3d32fdb3c49da3162254
四条眉毛 <fourfishxiake@gmail.com>
2021-07-25 20:02:36 +0800

    frame

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -19,18 +19,6 @@ type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
 
 #[tokio::main]
 async fn main() {
-  // let mut client = client::connect("127.0.0.1:6379").await.unwrap();
-  // let t1 = tokio::spawn(async {
-  //   let res = client.get("hello").await;
-  // });
-
-  // let t2 = tokio::spawn(async {
-  //   client.set("foo", "bar".into()).await;
-  // });
-
-  // t1.await.unwrap();
-  // t2.await.unwrap();
-
   let (tx, mut rx) = mpsc::channel(32);
 
   let manager = tokio::spawn(async move {
--- src/connection.rs
+++ src/connection.rs
@@ -1,16 +1,18 @@
 use tokio::net::TcpStream;
-use bytes::BytesMut;
-use mini_redis::{Frame, Result};
+use tokio::io::BufWriter;
+use bytes::{ BytesMut, Buf };
+use mini_redis::{Frame::{ Error::{ Incomplete } }, Result};
+use std::io::Cursor;
 
 struct Connection {
-  stream: TcpStream,
+  stream: BufWriter(TcpStream),
   buffer: BytesMut,
 }
 
 impl Connection {
   pub fn new(stream: TcpStream) -> Connection {
     Connection {
-      stream,
+      stream: BufWriter::new(stream),
       buffer: BytesMut::with_capacity(4096),
     }
   }
@@ -31,7 +33,70 @@ impl Connection {
     }
   }
 
-  pub async fn write_frame(&mut self, frame: &Frame) -> Result<()> {
+  fn parse_frame(&mut self) -> Result<Option<Frame>> {
+    let mut buf = Cursor::new(&self.buffer[..]);
+    match Frame::check(&mut buf) {
+      OK(_) => {
+        let len = buf.position() as usize;
 
+        buf.set_position(0);
+
+        let frame = Frame::parse(&mut buf)?;
+        self.buff.advance(len);
+        Ok(Some(frame))
+      },
+      Err(Incomplete) => Ok(None),
+      Err(e) => Err(e.into()),
+    }
+  }
+
+  pub async fn write_frame(&mut self, frame: &Frame) -> Result<()> {
+    match frame {
+      Frame::Simple(val) => {
+          self.stream.write_u8(b'+').await?;
+          self.stream.write_all(val.as_bytes()).await?;
+          self.stream.write_all(b"\r\n").await?;
+      }
+      Frame::Error(val) => {
+          self.stream.write_u8(b'-').await?;
+          self.stream.write_all(val.as_bytes()).await?;
+          self.stream.write_all(b"\r\n").await?;
+      }
+      Frame::Integer(val) => {
+          self.stream.write_u8(b':').await?;
+          self.write_decimal(*val).await?;
+      }
+      Frame::Null => {
+          self.stream.write_all(b"$-1\r\n").await?;
+      }
+      Frame::Bulk(val) => {
+          let len = val.len();
+
+          self.stream.write_u8(b'$').await?;
+          self.write_decimal(len as u64).await?;
+          self.stream.write_all(val).await?;
+          self.stream.write_all(b"\r\n").await?;
+      }
+      Frame::Array(_val) => unimplemented!(),
+    }
+
+    self.stream.flush().await;
+
+    Ok(())
+  }
+
+  async fn write_decimal(&mut self, val: u64) -> io::Result<()> {
+    use std::io::Write;
+
+    // Convert the value to a string
+    let mut buf = [0u8; 12];
+    let mut buf = Cursor::new(&mut buf[..]);
+    write!(&mut buf, "{}", val)?;
+
+    let pos = buf.position() as usize;
+    self.stream.write_all(&buf.get_ref()[..pos]).await?;
+    self.stream.write_all(b"\r\n").await?;
+
+    Ok(())
   }
 }
